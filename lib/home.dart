93af14034858200af8cb2d58ba1bdd7a12f3ac46
devpark435 <devpark435@gmail.com>
2023-12-08 14:43:58 +0900

    feat : appbar style update

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:profile_flutter_web/screens/introduce.dart';
 import 'package:profile_flutter_web/screens/profile.dart';
 import 'package:profile_flutter_web/screens/doing.dart';
+import 'package:profile_flutter_web/widget/typografie.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,17 +15,35 @@ class _HomeScreenState extends State<HomeScreen> {
   final _controller = PageController();
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-    final height = MediaQuery.of(context).size.height;
     return Scaffold(
+        appBar: AppBar(
+          centerTitle: false,
+          title: Padding(
+            padding: const EdgeInsets.only(left: 40),
+            child: Typografie().displayMedium("Profile", Colors.white),
+          ),
+          actions: [
+            TextButton(
+                onPressed: () {},
+                child: Typografie().displaySmall("text", Colors.white)),
+            TextButton(
+                onPressed: () {},
+                child: Typografie().displaySmall("text", Colors.white)),
+            TextButton(
+                onPressed: () {},
+                child: Typografie().displaySmall("text", Colors.white))
+          ],
+          backgroundColor: Colors.grey.shade800,
+          elevation: 0,
+        ),
         body: PageView(
-      controller: _controller,
-      scrollDirection: Axis.vertical,
-      children: [
-        ProfileScreen(),
-        IntroducScreen(),
-        DoingScreen(),
-      ],
-    ));
+          controller: _controller,
+          scrollDirection: Axis.vertical,
+          children: const [
+            ProfileScreen(),
+            IntroducScreen(),
+            DoingScreen(),
+          ],
+        ));
   }
 }
